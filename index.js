07d35c3095a61edb979359a165a18dc86e44eeb7
Jacob <weaver.jacob.t@gmail.com>
2023-10-19 18:43:51 -0500

    I didn't like this one

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+
+function upperCaseCustomerName(){
+  return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  return window.bestCustomer = 'not bob';
+  
+}
+
+function overwriteBestCustomer() {
+  return bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'bob';
+
+
+ function changeLeastFavoriteCustomer() {
+  return leastFavoriteCustomer = 'not bob';
+ }
